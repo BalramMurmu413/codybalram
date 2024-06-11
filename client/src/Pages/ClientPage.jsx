54dfcac6bfcd810485ce4df284561fa323a3798b
Balram Murmu <balramm413@gmail.com>
2024-06-11 12:03:46 +0530

    worked on clien page to make card

--- client/src/Pages/ClientPage.jsx
+++ client/src/Pages/ClientPage.jsx
@@ -11,32 +11,198 @@ export default function ClientPage() {
   return (
     <>
       <Header/>
-    <section className=' w-full h-screen'>
+    <section className=' w-full '>
             
     <div >
                   <div className=' w-10/12   m-auto flex flex-row flex-wrap items-center justify-around'>
                       {/* card one starting */}
             <div className=' w-44 md:w-64  border-2 hover:scale-125 duration-100 cursor-default '>
-                        <div className='flex mx- my- flex-col'>
+                        <div className='flex  my- flex-col'>
                             <div className=' flex flex-row justify-around items-center'>
-                                  <img src={sample} width={100} height={100} alt="" className=' rounded-full' />
+                                  <img src={sample} width={80} height={100} alt="" className=' rounded-full flex items-center justify-center' />
                                   <div className='flex flex-col items-start'>
                                   <h3 className='text-pink-500 md:text-lg text-sm'>Organisation Name</h3>
                                   <h3 className='text-white text-2xl'> *  *  * </h3>
                                   </div>
                             </div>
                             <div>
-                                  <h1 className='text-white font-bold'>Review</h1>
+                                  <h1 className='text-white font-bold text-sm md:text-lg' >Review</h1>
                             <p className='text-white italic'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, perspiciatis.</p>      
                               </div>
                               <div>
                                   <ul className='flex flex-row justify-around'>
                                       <li className='flex flex-row items-center text-white'>
-                                          <BiLike />
+                                          <BiLike className='cursor-pointer' />
                                           <span>2</span>
                                       </li>
                                       <li className='flex flex-row items-center text-white'>
-                                          <BiDislike />
+                                          <BiDislike className='cursor-pointer' />
+                                          <span>0</span>
+                                      </li>
+                                  </ul>
+                              </div>
+                          </div>
+                        </div>
+            {/* card ending */}
+
+
+               {/* card one starting */}
+               <div className=' w-44 md:w-64  border-2 hover:scale-125 duration-100 cursor-default '>
+                        <div className='flex  my- flex-col'>
+                            <div className=' flex flex-row justify-around items-center'>
+                                  <img src={sample} width={80} height={100} alt="" className=' rounded-full flex items-center justify-center' />
+                                  <div className='flex flex-col items-start'>
+                                  <h3 className='text-pink-500 md:text-lg text-sm'>Organisation Name</h3>
+                                  <h3 className='text-white text-2xl'> *  *  * </h3>
+                                  </div>
+                            </div>
+                            <div>
+                                  <h1 className='text-white font-bold text-sm md:text-lg' >Review</h1>
+                            <p className='text-white italic'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, perspiciatis.</p>      
+                              </div>
+                              <div>
+                                  <ul className='flex flex-row justify-around'>
+                                      <li className='flex flex-row items-center text-white'>
+                                          <BiLike className='cursor-pointer' />
+                                          <span>2</span>
+                                      </li>
+                                      <li className='flex flex-row items-center text-white'>
+                                          <BiDislike className='cursor-pointer' />
+                                          <span>0</span>
+                                      </li>
+                                  </ul>
+                              </div>
+                          </div>
+                        </div>
+            {/* card ending */}
+
+
+
+
+
+               {/* card one starting */}
+               <div className=' w-44 md:w-64  border-2 hover:scale-125 duration-100 cursor-default '>
+                        <div className='flex mx- my- flex-col'>
+                            <div className=' flex flex-row justify-around items-center'>
+                                  <img src={sample} width={80} height={100} alt="" className=' rounded-full flex items-center justify-center' />
+                                  <div className='flex flex-col items-start'>
+                                  <h3 className='text-pink-500 md:text-lg text-sm'>Organisation Name</h3>
+                                  <h3 className='text-white text-2xl'> *  *  * </h3>
+                                  </div>
+                            </div>
+                            <div>
+                                  <h1 className='text-white font-bold text-sm md:text-lg' >Review</h1>
+                            <p className='text-white italic'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, perspiciatis.</p>      
+                              </div>
+                              <div>
+                                  <ul className='flex flex-row justify-around'>
+                                      <li className='flex flex-row items-center text-white'>
+                                          <BiLike className='cursor-pointer' />
+                                          <span>2</span>
+                                      </li>
+                                      <li className='flex flex-row items-center text-white'>
+                                          <BiDislike className='cursor-pointer' />
+                                          <span>0</span>
+                                      </li>
+                                  </ul>
+                              </div>
+                          </div>
+                        </div>
+            {/* card ending */}
+
+
+
+
+
+               {/* card one starting */}
+               <div className=' w-44 md:w-64  border-2 hover:scale-125 duration-100 cursor-default '>
+                        <div className='flex mx- my- flex-col'>
+                            <div className=' flex flex-row justify-around items-center'>
+                                  <img src={sample} width={80} height={100} alt="" className=' rounded-full flex items-center justify-center' />
+                                  <div className='flex flex-col items-start'>
+                                  <h3 className='text-pink-500 md:text-lg text-sm'>Organisation Name</h3>
+                                  <h3 className='text-white text-2xl'> *  *  * </h3>
+                                  </div>
+                            </div>
+                            <div>
+                                  <h1 className='text-white font-bold text-sm md:text-lg' >Review</h1>
+                            <p className='text-white italic'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, perspiciatis.</p>      
+                              </div>
+                              <div>
+                                  <ul className='flex flex-row justify-around'>
+                                      <li className='flex flex-row items-center text-white'>
+                                          <BiLike className='cursor-pointer' />
+                                          <span>2</span>
+                                      </li>
+                                      <li className='flex flex-row items-center text-white'>
+                                          <BiDislike className='cursor-pointer' />
+                                          <span>0</span>
+                                      </li>
+                                  </ul>
+                              </div>
+                          </div>
+                        </div>
+            {/* card ending */}
+
+
+
+
+
+               {/* card one starting */}
+               <div className='  w-44 md:w-64  border-2 hover:scale-105 duration-100 cursor-default '>
+                        <div className='flex mx-5 my- flex-col'>
+                            <div className=' flex flex-row justify-around items-center'>
+                                  <img src={sample} width={80} height={100} alt="" className=' rounded-full flex items-center justify-center' />
+                                  <div className='flex flex-col items-start'>
+                                  <h3 className='text-pink-500 md:text-lg text-sm'>Organisation Name</h3>
+                                  <h3 className='text-white text-2xl'> *  *  * </h3>
+                                  </div>
+                            </div>
+                            <div>
+                                  <h1 className='text-white font-bold text-sm md:text-lg' >Review</h1>
+                            <p className='text-white italic'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, perspiciatis.</p>      
+                              </div>
+                              <div>
+                                  <ul className='flex flex-row justify-around'>
+                                      <li className='flex flex-row items-center text-white'>
+                                          <BiLike className='cursor-pointer' />
+                                          <span>2</span>
+                                      </li>
+                                      <li className='flex flex-row items-center text-white'>
+                                          <BiDislike className='cursor-pointer' />
+                                          <span>0</span>
+                                      </li>
+                                  </ul>
+                              </div>
+                          </div>
+                        </div>
+            {/* card ending */}
+
+
+
+
+               {/* card one starting */}
+               <div className=' w-44 md:w-64  border-2 hover:scale-125 duration-100 cursor-default '>
+                        <div className='flex mx-5 my- flex-col'>
+                            <div className=' flex flex-row justify-around items-center'>
+                                  <img src={sample} width={80} height={100} alt="" className=' rounded-full flex items-center justify-center' />
+                                  <div className='flex flex-col items-start'>
+                                  <h3 className='text-pink-500 md:text-lg text-sm'>Organisation Name</h3>
+                                  <h3 className='text-white text-2xl'> *  *  * </h3>
+                                  </div>
+                            </div>
+                            <div>
+                                  <h1 className='text-white font-bold text-sm md:text-lg' >Review</h1>
+                            <p className='text-white italic'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, perspiciatis.</p>      
+                              </div>
+                              <div>
+                                  <ul className='flex flex-row justify-around'>
+                                      <li className='flex flex-row items-center text-white'>
+                                          <BiLike className='cursor-pointer' />
+                                          <span>2</span>
+                                      </li>
+                                      <li className='flex flex-row items-center text-white'>
+                                          <BiDislike className='cursor-pointer' />
                                           <span>0</span>
                                       </li>
                                   </ul>
--- client/src/Pages/OurWork.jsx
+++ client/src/Pages/OurWork.jsx
@@ -1,28 +1,36 @@
 import React from 'react'
 import Header from '../Components/Header.jsx'
 import SimpleFooter from '../Components/SimpleFooter.jsx'
+import computer from '/Images/computer.jpg'
+import { Link } from 'react-router-dom'
 
 export default function OurWork() {
   return (
       <>
           <Header/>
-          <section className='h-screen'>
-              <div>
-                  <h1>Our Work</h1>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, quas.</p>
-                  <div>
-                      <h2>Lorem ipsum dolor sit amet.</h2>
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, quas.</p>
-                  </div>
-                  <div>
-                      <h2>Lorem ipsum dolor sit amet.</h2>
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, quas.</p>
-                  </div>
-                  <div>
-                      <h2>Lorem ipsum dolor sit amet.</h2>
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, quas.</p>
-                  </div>
-              </div>
+          <section className='w-full  text-white flex-col'>
+            {/* my work card */}
+            <div className='w-full flex flex-row bg-slate-500 min-h-50'>
+                  <div className='mr-5'>
+                      <img src={computer} alt="" width={400} />
+</div>
+                  <div className='w-full my-2 mx-2'>
+                      <div className='w-full mr-16 flex justify-between'>
+                          <h1 className='text-pink-300 font-bold'>Walkman</h1>
+                          <Link to='https://walkmanproject1.pages.dev/ ' target='_black'>
+                          <button className='py-1 px-4 rounded-md bg-blue-400 text-white font-semibold '>Visit</button>
+                          </Link>
+                      </div>
+
+
+                      <div className='flex flex-col w-full items-start'>
+                          <h1>Helow </h1>
+                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, mollitia quae! Eum.</p>
+                      </div>
+</div>
+
+
+            </div>
       </section>
       <SimpleFooter/>
           
