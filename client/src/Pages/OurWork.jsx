ad9cdfc16dd76d174e6d795b266599ffcd9d2d1e
Balram Murmu <balramm413@gmail.com>
2024-06-11 18:00:41 +0530

    worked on clien page to make card

--- client/src/Pages/OurWork.jsx
+++ client/src/Pages/OurWork.jsx
@@ -8,29 +8,133 @@ export default function OurWork() {
   return (
       <>
           <Header/>
-          <section className='w-full  text-white flex-col'>
-            {/* my work card */}
-            <div className='w-full flex flex-row bg-slate-500 min-h-50'>
+          <section className='w-full min-h-svh  text-white flex-col'>
+           
+        {/* my work card */}
+            <div className='w-full my-2 flex flex-row bg-slate-700 min-h-'>
                   <div className='mr-5'>
                       <img src={computer} alt="" width={400} />
-</div>
+                  </div>
                   <div className='w-full my-2 mx-2'>
                       <div className='w-full mr-16 flex justify-between'>
-                          <h1 className='text-pink-300 font-bold'>Walkman</h1>
-                          <Link to='https://walkmanproject1.pages.dev/ ' target='_black'>
-                          <button className='py-1 px-4 rounded-md bg-blue-400 text-white font-semibold '>Visit</button>
+                          <h1 className='text-pink-300 font-bold'>Beats.com</h1>
+                          <Link to='https://walkmanproject1.pages.dev/ ' target='_blank'>
+                          <button className='py-1 px-4 rounded-md bg-pink-400 text-white font-semibold '>Visit</button>
                           </Link>
                       </div>
-
-
                       <div className='flex flex-col w-full items-start'>
-                          <h1>Helow </h1>
-                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, mollitia quae! Eum.</p>
+                          <h1> This was my first Portfolio website </h1>
+              <p className=' place-content-start'>
+                <span className='font-bold'>Used frameworks : </span>  
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores illum laboriosam quos at cumque ea.</p>
                       </div>
-</div>
-
-
+          </div>
             </div>
+          {/* ending of card */}
+
+        
+          {/* my work card */}
+          <div className='w-full my-2 flex flex-row bg-slate-700 min-h-'>
+                  <div className='mr-5'>
+                      <img src={computer} alt="" width={400} />
+                  </div>
+                  <div className='w-full my-2 mx-2'>
+                      <div className='w-full mr-16 flex justify-between'>
+                          <h1 className='text-pink-300 font-bold'>codingtag.com</h1>
+                          <Link to='https://codingtag.pages.dev/ ' target='_blank'>
+                          <button className='py-1 px-4 rounded-md bg-pink-400 text-white font-semibold '>Visit</button>
+                          </Link>
+                      </div>
+                      <div className='flex flex-col w-full items-start'>
+                          <h1> This is an E-commerce website, listed the items of Beats electronic product to purchase online  </h1>
+              <p className=' place-content-start'>
+                <span className='font-bold'>Used frameworks : </span>  
+                This website is developed by HTML, CSS, and Javascript and have very poor functionality becouse this was my first website i have made....</p>
+                      </div>
+          </div>
+            </div>
+          {/* ending of card */}
+
+        
+
+
+
+          {/* my work card */}
+          <div className='w-fullmy-2  flex flex-row bg-slate-700 min-h-'>
+                  <div className='mr-5'>
+                      <img src={computer} alt="" width={400} />
+                  </div>
+                  <div className='w-full my-2 mx-2'>
+                      <div className='w-full mr-16 flex justify-between'>
+                          <h1 className='text-pink-300 font-bold'>Beats.com</h1>
+                          <Link to='https://walkmanproject1.pages.dev/ ' target='_black'>
+                          <button className='py-1 px-4 rounded-md bg-pink-400 text-white font-semibold '>Visit</button>
+                          </Link>
+                      </div>
+                      <div className='flex flex-col w-full items-start'>
+                          <h1> This is an E-commerce website, listed the items of Beats electronic product to purchase online  </h1>
+              <p className=' place-content-start'>
+                <span className='font-bold'>Used frameworks : </span>  
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum dignissimos quidem quae, velit praesentium ab quia voluptatibus minus facere culpa.</p>
+                      </div>
+          </div>
+            </div>
+          {/* ending of card */}
+
+        
+
+
+
+          {/* my work card */}
+          <div className='w-full my-2  flex flex-row bg-slate-700 min-h-'>
+                  <div className='mr-5'>
+                      <img src={computer} alt="" width={400} />
+                  </div>
+                  <div className='w-full my-2 mx-2'>
+                      <div className='w-full mr-16 flex justify-between'>
+                          <h1 className='text-pink-300 font-bold'>Beats.com</h1>
+                          <Link to='https://walkmanproject1.pages.dev/ ' target='_black'>
+                          <button className='py-1 px-4 rounded-md bg-pink-400 text-white font-semibold '>Visit</button>
+                          </Link>
+                      </div>
+                      <div className='flex flex-col w-full items-start'>
+                          <h1> This is an E-commerce website, listed the items of Beats electronic product to purchase online  </h1>
+              <p className=' place-content-start'>
+                <span className='font-bold'>Used frameworks : </span>  
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum dignissimos quidem quae, velit praesentium ab quia voluptatibus minus facere culpa.</p>
+                      </div>
+          </div>
+            </div>
+          {/* ending of card */}
+
+        
+
+
+
+
+          {/* my work card */}
+          <div className='w-full my-2 flex flex-row bg-slate-700 min-h-'>
+                  <div className='mr-5'>
+                      <img src={computer} alt="" width={400} />
+                  </div>
+                  <div className='w-full my-2 mx-2'>
+                      <div className='w-full mr-16 flex justify-between'>
+                          <h1 className='text-pink-300 font-bold'>Beats.com</h1>
+                          <Link to='https://walkmanproject1.pages.dev/ ' target='_black'>
+                          <button className='py-1 px-4 rounded-md bg-pink-400 text-white font-semibold '>Visit</button>
+                          </Link>
+                      </div>
+                      <div className='flex flex-col w-full items-start'>
+                          <h1> This is an E-commerce website, listed the items of Beats electronic product to purchase online  </h1>
+              <p className=' place-content-start'>
+                <span className='font-bold'>Used frameworks : </span>  
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum dignissimos quidem quae, velit praesentium ab quia voluptatibus minus facere culpa.</p>
+                      </div>
+          </div>
+            </div>
+          {/* ending of card */}
+
+
       </section>
       <SimpleFooter/>
           
